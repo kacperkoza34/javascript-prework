dd4d9df53061a73e0583e77473d12d584cf8ac7b
Kacper Koza <kacperkoza34@gmail.com>
2019-07-05 23:46:25 +0200

    Add function, delete code from script

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,38 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  }
+	else if(argMoveId == 2){
+		return 'papier';
+	}
+	else if(argMoveId == 3){
+		return 'nożyce';
+	}
+
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+
+	if (argComputerMove == argPlayerMove){
+    printMessage('Remis!');
+  }
+  else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+    printMessage('Przegrałeś!!!');
+  }
+  else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+    printMessage('Przegrałeś!!!');
+  }
+  else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+    printMessage('Przegrałeś!!!');
+  }
+  else if (argPlayerMove == 'nieznany ruch') {
+    printMessage('Wybierz F5 i podaj poprawną wartość');
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,52 +1,7 @@
-let computerMove = `nieznana liczba`;
-let playerMove = `nieznana liczba`;
-
-// computer move
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if(randomNumber == 2){
-  computerMove = 'papier';
-}
-else if(randomNumber == 3){
-  computerMove = 'nożyce';
-}
-
-
-// player player move
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-if(playerInput == 1){
-  playerMove = 'kamień';
-}
-else if(playerInput == 2){
-  playerMove = 'papier';
-}
-else if(playerInput == 3){
-  playerMove = 'nożyce';
-}
 
+let computerMove = getMoveName(randomNumber);
+let playerMove = getMoveName(playerInput);
 
-
-  // result
-if(playerInput == 1 || playerInput ==2 || playerInput ==3 ){
-  printMessage('Wybrałeś ' + playerMove + '! Przeciwnik wybrał ' + computerMove + '!');
-  if (computerMove == playerMove){
-    printMessage('Remis!');
-  }
-  else if (computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Przegrałeś!!!');
-  }
-  else if (computerMove == 'nożyce' && playerMove == 'papier'){
-    printMessage('Przegrałeś!!!');
-  }
-  else if (computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Przegrałeś!!!');
-  }
-  else {
-    printMessage('Wygrałeś!!!');
-  }
-}
-else{
-  printMessage('Nieznany znak')
-}
+displayResult(computerMove, playerMove);
