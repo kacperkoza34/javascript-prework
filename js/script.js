f1d2da5158a3a91f49a9f692e403111202bb5f13
Kacper Koza <kacperkoza34@gmail.com>
2019-07-05 11:29:20 +0200

    Add let playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 let computerMove = `kamień`;
+let playerMove = `papier`;
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
