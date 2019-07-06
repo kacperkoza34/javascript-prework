9ab8f605438ad7e9512ba1193076a44374cbaa98
Kacper Koza <kacperkoza34@gmail.com>
2019-07-06 14:05:54 +0200

    Add bootstrap

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+function playGame(playerInput){
+	clearMessages();
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerMove = getMoveName(randomNumber);
+  let playerMove = getMoveName(playerInput);
+  displayResult(computerMove, playerMove);
+}
 
-let computerMove = getMoveName(randomNumber);
-let playerMove = getMoveName(playerInput);
-
-displayResult(computerMove, playerMove);
+document.getElementById('play-rock').addEventListener('click', playGame);
