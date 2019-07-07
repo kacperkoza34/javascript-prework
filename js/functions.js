9bf54b867cc3b7207a87c9d5e57c8ac82c3b84b2
Kacper Koza <kacperkoza34@gmail.com>
2019-07-07 13:04:07 +0200

    Add on click event, Add icons

--- js/functions.js
+++ js/functions.js
@@ -45,3 +45,11 @@ function displayResult(argComputerMove, argPlayerMove){
 		printMessage('Wygrałeś!!!');
 	}
 }
+
+function playGame(playerInput){
+	clearMessages();
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerMove = getMoveName(randomNumber);
+  let playerMove = getMoveName(playerInput);
+  displayResult(computerMove, playerMove);
+}
--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-function playGame(playerInput){
-	clearMessages();
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  let computerMove = getMoveName(randomNumber);
-  let playerMove = getMoveName(playerInput);
-  displayResult(computerMove, playerMove);
-}
 
-document.getElementById('play-rock').addEventListener('click', playGame);
+
+document.getElementById('play-rock').addEventListener('click', function (){ playGame(1) });
+document.getElementById('play-paper').addEventListener('click', function (){ playGame(2) });
+document.getElementById('play-scissors').addEventListener('click', function (){ playGame(3) });
